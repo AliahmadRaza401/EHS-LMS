d007a43a3791e7e23b194f93268d83587ebc0326
hbsgujjar111 <hbsgujjar111h@gmail.com>
2021-07-16 17:02:44 +0500

    hassan

--- lib/Screens/Splash/splashScreen.dart
+++ lib/Screens/Splash/splashScreen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      Duration(seconds: 300),
+      Duration(seconds: 3),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (BuildContext context) => SelectRole(),
