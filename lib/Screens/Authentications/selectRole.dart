a1402a834a8f1e1c01642f68783cef15c1ca4a36
Your Name <you@example.com>
2021-07-16 16:16:06 +0500

    raza

---
+++ lib/Screens/Authentications/selectRole.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SelectRole extends StatefulWidget {
+  // SelectRole({Key? key}) : super(key: key);
+
+  @override
+  _SelectRoleState createState() => _SelectRoleState();
+}
+
+class _SelectRoleState extends State<SelectRole> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Role"),
+    );
+  }
+}
---
+++ lib/Screens/Splash/splashScreen.dart
@@ -0,0 +1,29 @@
+import 'package:ehs_lms/Screens/Authentications/selectRole.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => SelectRole())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+  }
+}
