3ba95cc859614a65eadd1be741455dcb047b0043
hbsgujjar111 <hbsgujjar111h@gmail.com>
2021-07-17 19:52:36 +0500

    hassan

--- lib/Screens/Authentications/login.dart
+++ lib/Screens/Authentications/login.dart
@@ -1,4 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:ehs_lms/widgets/auth_bg.dart';
 import 'package:flutter/material.dart';
+
+final email = TextEditingController();
+final password = TextEditingController();
+final _formKey = GlobalKey<FormState>();
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,67 +17,273 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: RadialGradient(
-            radius: 0.9,
-            colors: [
-              Color(0xffffffff),
-              Color(0xff6EB7C6),
-            ],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/logo.png",
-                  width: MediaQuery.of(context).size.width * 0.4,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [centerContainer()],
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          auth_bg(context),
+          centerContainer(),
+        ],
       ),
     );
   }
 
   Widget centerContainer() {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.7,
-      height: MediaQuery.of(context).size.height * 0.4,
-      decoration: BoxDecoration(
-        gradient: RadialGradient(
-          radius: 0.9,
-          colors: [
-            Color(0xff23FFFF).withOpacity(0.68),
-            Color(0xff2D91A5).withOpacity(0.68),
-          ],
+    return Center(
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * .86,
+                    height: MediaQuery.of(context).size.height * .44,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(36.0),
+                      gradient: RadialGradient(
+                        radius: 0.9,
+                        colors: [
+                          Color(0xff23FFFF).withOpacity(0.68),
+                          Color(0xff2D91A5).withOpacity(0.68),
+                        ],
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * .04,
+                      horizontal: MediaQuery.of(context).size.width * .14,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Flexible(
+                              child: emailBar(context),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: passwordBar(context),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 1,
+                                height: 50.0,
+                                child: OutlinedButton(
+                                  onPressed: null,
+                                  child: AutoSizeText(
+                                    "Sign In",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                    ),
+                                    maxLines: 1,
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                    side: BorderSide(
+                                      color: Color(0xff1EB2A6),
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(9.0),
+                                      ),
+                                    ),
+                                    backgroundColor: Color(0xff1EB2A6),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Forgot Password",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Or",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Register with",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/google_auth_icon.png",
+                                  width: 32.0,
+                                  height: 32.0,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Column(
-        children: [
-          TextField(
-            // controller: _controller,
-            decoration: InputDecoration(
-              hintText: "Enter a message",
-              suffixIcon: Icon(Icons.clear),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("SignIn"),
-          ),
-        ],
       ),
     );
   }
+}
+
+//email widget
+Widget emailBar(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width * 1,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(9),
+    ),
+    child: TextFormField(
+      initialValue: null,
+      autocorrect: true,
+      controller: email,
+      validator: (query) {
+        if (query!.isEmpty) {
+          return 'Search Some Advice!';
+        } else {
+          return null;
+        }
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      keyboardAppearance: Brightness.dark,
+      keyboardType: TextInputType.name,
+      style: TextStyle(
+        color: Color(0xffaeaeae),
+        fontSize: 15.0,
+        decoration: TextDecoration.none,
+      ),
+      textInputAction: TextInputAction.next,
+      cursorColor: Colors.white,
+      cursorWidth: 2.0,
+      cursorHeight: 26.0,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(vertical: 22.0, horizontal: 20.0),
+        errorStyle: TextStyle(
+          fontSize: 15.0,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(9.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(9.0),
+        ),
+        border: InputBorder.none,
+        hintText: 'Email',
+        hintStyle: TextStyle(
+          color: Color(0xffaeaeae),
+          fontSize: 15.0,
+        ),
+        suffixIcon: Icon(
+          Icons.mail_outline_rounded,
+          color: Color(0xffaeaeae),
+        ),
+      ),
+    ),
+  );
+}
+
+//email widget
+Widget passwordBar(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width * 1,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(9),
+    ),
+    child: TextFormField(
+      initialValue: null,
+      autocorrect: true,
+      controller: password,
+      validator: (query) {
+        if (query!.isEmpty) {
+          return 'Search Some Advice!';
+        } else {
+          return null;
+        }
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      keyboardAppearance: Brightness.dark,
+      keyboardType: TextInputType.name,
+      style: TextStyle(
+        color: Color(0xffaeaeae),
+        fontSize: 15.0,
+        decoration: TextDecoration.none,
+      ),
+      textInputAction: TextInputAction.next,
+      cursorColor: Colors.white,
+      cursorWidth: 2.0,
+      cursorHeight: 26.0,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(vertical: 22.0, horizontal: 20.0),
+        errorStyle: TextStyle(
+          fontSize: 15.0,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(9.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(9.0),
+        ),
+        border: InputBorder.none,
+        hintText: 'Password',
+        hintStyle: TextStyle(
+          color: Color(0xffaeaeae),
+          fontSize: 15.0,
+        ),
+        suffixIcon: Image.asset(
+          "assets/images/password_auth_icon.png",
+          color: Color(0xffaeaeae),
+          width: 12.0,
+          height: 12.0,
+        ),
+      ),
+    ),
+  );
 }
--- lib/Screens/Authentications/selectRole.dart
+++ lib/Screens/Authentications/selectRole.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:ehs_lms/widgets/auth_bg.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -25,8 +26,8 @@ class _SelectRoleState extends State<SelectRole> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: MediaQuery.of(context).size.width * .9,
-                    height: MediaQuery.of(context).size.height * .4,
+                    width: MediaQuery.of(context).size.width * .86,
+                    height: MediaQuery.of(context).size.height * .34,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(36.0),
                       gradient: RadialGradient(
@@ -37,23 +38,33 @@ class _SelectRoleState extends State<SelectRole> {
                         ],
                       ),
                     ),
-                    padding: EdgeInsets.all(20.0),
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * .04,
+                      horizontal: MediaQuery.of(context).size.width * .14,
+                    ),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        
-                        Row(
-                          children: [
-                            OutlinedButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => Login(),
-                                  ),
-                                );
-                              },
-                              child: Text("Hi"),
-                            ),
-                          ],
+                        loginPattern(
+                          context,
+                          "assets/images/student_auth_icon.png",
+                          "Student",
+                        ),
+                        Divider(
+                          color: Colors.white,
+                        ),
+                        loginPattern(
+                          context,
+                          "assets/images/teacher_auth_icon.png",
+                          "Teacher",
+                        ),
+                        Divider(
+                          color: Colors.white,
+                        ),
+                        loginPattern(
+                          context,
+                          "assets/images/parent_auth_icon.png",
+                          "Parents",
                         ),
                       ],
                     ),
@@ -66,4 +77,55 @@ class _SelectRoleState extends State<SelectRole> {
       ),
     );
   }
+}
+
+Widget loginPattern(BuildContext context, String typeIcon, type) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => Login(),
+        ),
+      );
+    },
+    child: Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Image.asset(
+                typeIcon,
+                color: Colors.white,
+                width: 22.0,
+                height: 22.0,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              AutoSizeText(
+                "Sign in as $type",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.asset(
+                "assets/images/next_auth.png",
+                color: Colors.white,
+                width: 18.0,
+                height: 18.0,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
