b46a3a6ee69187d05b5f52a112a1319badefac61
Your Name <you@example.com>
2021-07-28 15:07:56 +0500

    raza

--- lib/Screens/app_Screens/bottomNav/bottomNav.dart
+++ lib/Screens/app_Screens/bottomNav/bottomNav.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:ehs_lms/Screens/app_Screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation_action_bar/navigation_action_bar.dart';
@@ -16,7 +18,6 @@ class _BottomNavState extends State<BottomNav> {
     HomeScreen(),
     Text('Index 2: Events'),
     Text('Index 3: Profile'),
-    Text('Index 4: Menu'),
     Text('Index 2: Events'),
     Text('Index 3: Profile'),
     Text('Index 4: Menu'),
@@ -31,23 +32,70 @@ class _BottomNavState extends State<BottomNav> {
     return Scaffold(
       bottomNavigationBar: NavigationActionBar(
         context: context,
-        scaffoldColor: Colors.blueAccent,
+        accentColor: Color(0xff6EB7C6),
+        scaffoldColor: Color(0xff7DC3E2),
+        backgroundColor: Color(0xff6EB7C6),
+        //  Color(0xff7DD1E2),
+        //     Color(0xff7DC3E2),
         index: 0,
         subItems: [
-          NavBarItem(iconData: Icons.attach_file, size: 25),
-          NavBarItem(iconData: Icons.photo, size: 25),
-          NavBarItem(iconData: Icons.camera_alt, size: 25),
-          NavBarItem(iconData: Icons.attach_file, size: 25),
+          NavBarItem(
+            iconData: Icons.attach_file,
+            size: 25,
+          ),
+          NavBarItem(
+              iconData: Icons.photo,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.camera_alt,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.attach_file,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
         ],
         mainIndex: 3,
         items: [
-          NavBarItem(iconData: Icons.person, size: 30),
-          NavBarItem(iconData: Icons.list, size: 30),
-          NavBarItem(iconData: Icons.compare_arrows, size: 30),
-          NavBarItem(iconData: Icons.arrow_upward_outlined, size: 40),
-          NavBarItem(iconData: Icons.call_merge, size: 30),
-          NavBarItem(iconData: Icons.person, size: 30),
-          NavBarItem(iconData: Icons.person, size: 30),
+          NavBarItem(
+              iconData: Icons.home,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.list,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.compare_arrows,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.keyboard_arrow_up_outlined,
+              size: 40,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.call_merge,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.person,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
+          NavBarItem(
+              iconData: Icons.person,
+              size: 35,
+              selectedColor: Color(0xff23FFFF),
+              unselectedColor: Colors.white),
         ],
         onTap: (index) {
           setState(() {
@@ -56,7 +104,6 @@ class _BottomNavState extends State<BottomNav> {
         },
       ),
       body: Container(
-        color: Colors.blueAccent,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Center(
