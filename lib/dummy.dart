61d1508f7532cb3acb93a77489c03500ae30a4cb
Your Name <you@example.com>
2021-07-29 15:25:17 +0500

    raza

--- lib/dummy.dart
+++ lib/dummy.dart
@@ -13,40 +13,102 @@ class _DummyState extends State<Dummy> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Header(context),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment(-0.76, -0.1),
+            radius: 1.1,
+            colors: [
+              Color(0xffffffff),
+              Color(0xff6EB7C6),
             ],
+            stops: <double>[0.0, 1.0],
           ),
-          body(),
-        ],
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Header(context),
+              ],
+            ),
+            body(),
+          ],
+        ),
       ),
     );
   }
 
   Widget body() {
     return Container(
-      color: Colors.white,
-      width: MediaQuery.of(context).size.width * .95,
-      padding: EdgeInsets.symmetric(vertical: 50),
+      width: MediaQuery.of(context).size.width * .97,
       child: Column(
         children: [
           Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-              color: Colors.amber,
-              boxShadow: [
-                BoxShadow(color: Colors.green, spreadRadius: 3),
-              ],
+              color: Color(0xff558D98),
             ),
             child: Row(
               children: [
                 Column(
-                  children: [Text("Home")],
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        Text(
+                          "Home",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.supervised_user_circle_rounded,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        Text(
+                          "Profile",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.social_distance,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        Text(
+                          "Social Learning",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
